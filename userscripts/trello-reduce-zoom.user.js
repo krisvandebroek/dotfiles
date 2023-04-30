dc108bbbbe1d04d4f7f36706b1f8e3d0a106106b
Kris Vandebroek <kris@vandebroek.net>
2023-04-30 18:10:13 +0200

    Added userscripts

---
+++ userscripts/trello-reduce-zoom.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name          Reduce Zoom
+// @version       1.0.0
+// @homepage      https://github.com/krisvandebroek/dotfiles/
+// @description   Reduce Zoom to 90% to fit more on screen
+// @match         https://trello.com/b/*
+// @match         http://trello.com/b/*
+// @updateUrl     https://github.com/krisvandebroek/dotfiles/raw/main/userscripts/trello-reduce-zoom.user.js
+// @downloadUrl   https://github.com/krisvandebroek/dotfiles/raw/main/userscripts/trello-reduce-zoom.user.js
+// ==/UserScript==
+
+document.body.style.zoom = "90%";
